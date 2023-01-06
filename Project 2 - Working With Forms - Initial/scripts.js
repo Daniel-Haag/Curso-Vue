05172cba44ed139e63e7fdfd087c7dc65488185d
Daniel Haag <danni.haag91@gmail.com>
2023-01-05 21:54:09 -0300

    primeiro commit

---
+++ Project 2 - Working With Forms - Initial/scripts.js
@@ -0,0 +1,24 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Violão'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Guitarra'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members
+        }
+    },
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
