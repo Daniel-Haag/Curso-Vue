96b60b28e91efc654fd8e208623811504c788819
Daniel Haag <danni.haag91@gmail.com>
2023-02-04 23:22:09 -0300

    Configurando a aplicação para fazer requisições HTTP com o axios

--- project4-quiz-game-initial/src/main.js
+++ project4-quiz-game-initial/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app").use(VueAxios, axios);
